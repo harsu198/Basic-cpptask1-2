b91e8b161d2c30918fb0f68d6ab043beb004c15d
harsu198 <62491971+harsu198@users.noreply.github.com>
2020-04-28 14:49:38 +0530

    create Product of two number.cpp
    if we product two number as
    4.5*4.5 then = 20.25

---
+++ Product of two number.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	double firstnumber, secondnumber,productoftwonumber;
+	cout<<"Enter two number : ";
+	cin>>firstnumber>> secondnumber;
+	productoftwonumber = firstnumber*secondnumber;
+	cout<< "Procuct = "<< productoftwonumber;
+	return 0;
+}
